0cd0e78b3d6288b9530f94dc234380ab8097c97b
HelioStrike <rageofepicfury@gmail.com>
2018-12-24 16:32:35 +0530

    committed

--- v0/routes/index.js
+++ v0/routes/index.js
@@ -2,11 +2,11 @@ var express = require("express"),
     router = express.Router();
 
 router.get("/", function(req, res) {
-  res.render("index");
+  res.render("index", {title: "Sree Charit Hospitals - Exclusive Neuro endocrine Center"});
 });
 
 router.get("/doctors", function(req, res) {
-  res.render("doctors");
+  res.render("doctors",{title: "Doctors - Sree Charit Hospitals"});
 });
 
 module.exports = router;
